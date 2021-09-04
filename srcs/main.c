409338433a3d9cbb8c9029aa82a67e6096f7e7dd
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2021-09-04 18:28:50 -0300

    feat: destroy window when ESC or Window X is pressed

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,14 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 02:10:12 by gandrade          #+#    #+#             */
-/*   Updated: 2021/09/02 22:38:31 by gandrade         ###   ########.fr       */
+/*   Updated: 2021/09/03 21:33:39 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+static void	check_input_error(int argc, char *fractal);
+static void	parse_fractal(char *fractal);
 
 int	main(int argc, char *argv[])
 {
@@ -20,4 +23,38 @@ int	main(int argc, char *argv[])
 	check_input_error(argc, fractal);
 	parse_fractal(fractal);
 	exit(EXIT_SUCCESS);
+}
+
+static void	check_input_error(int argc, char *fractal)
+{
+	if (argc == 1)
+	{
+		printf("\n[ERROR]\n\n");
+		printf("Choose a fractal set: Mandelbrot or Julia.\n\n");
+		printf("Ex: ./fract-ol Mandelbrot\nEx: ./fract-ol Julia\n\n");
+		exit(EXIT_FAILURE);
+	}
+	if (argc > 2)
+	{
+		printf("\n[ERROR]\n\n");
+		printf("Choose just one fractal set: Mandelbrot or Julia.\n\n");
+		printf("Ex: ./fract-ol Mandelbrot\nEx: ./fract-ol Julia\n\n");
+		exit(EXIT_FAILURE);
+	}
+	if (!(ft_strcmp(fractal, "Mandelbrot") == 0
+			|| ft_strcmp(fractal, "Julia") == 0))
+	{
+		printf("\n[ERROR]\n\n");
+		printf("Choose a valid fractal set: Mandelbrot or Julia.\n\n");
+		printf("Ex: ./fract-ol Mandelbrot\nEx: ./fract-ol Julia\n\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void	parse_fractal(char *fractal)
+{
+	if (ft_strcmp(fractal, "Mandelbrot") == 0)
+		create_mandelbrot();
+	//if (ft_strcmp(fractal, "Julia") == 0)
+	//	create_julia();
 }
--- srcs/mandelbrot.c
+++ srcs/mandelbrot.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol.h                                          :+:      :+:    :+:   */
+/*   mandelbrot.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/27 02:09:53 by gandrade          #+#    #+#             */
-/*   Updated: 2021/09/03 21:34:01 by gandrade         ###   ########.fr       */
+/*   Created: 2021/09/03 11:16:00 by gandrade          #+#    #+#             */
+/*   Updated: 2021/09/03 12:24:38 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FRACTOL_H
-# define FRACTOL_H
+#include "fractol.h"
 
-# include "mlx.h"
-# include "libft.h"
-# include <math.h>
-# include <stdio.h>
-# include <fcntl.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-typedef struct s_mlx_vars
+void	create_mandelbrot(void)
 {
-	void	*mlx;
-	void	*win;
-}	t_mlx_vars;
-
-void	create_mandelbrot(void);
-//void	create_julia(void);
-void	mlx_handler(void);
-
-#endif
+	mlx_handler();
+}
