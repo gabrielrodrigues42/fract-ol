bedd8c2c5f8d4b6703053afacef923175eebb17c
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2021-10-08 21:25:23 -0300

    fix: new limits

--- srcs/parse_args.c
+++ srcs/parse_args.c
@@ -6,7 +6,7 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 18:55:49 by gandrade          #+#    #+#             */
-/*   Updated: 2021/10/08 17:06:02 by gandrade         ###   ########.fr       */
+/*   Updated: 2021/10/08 17:48:07 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	parse_args(int argc, char **argv, t_vars *vars)
 		print_help();
 	else if (ft_strcmp((ft_strtolower(vars->set)), "mandelbrot") == 0)
 		vars->calculate = &calculate_mandelbrot;
-	else if (ft_strcmp((ft_strtolower(vars->set)), "burningship") == 0)
+	else if (ft_strcmp((ft_strtolower(vars->set)), "burning-ship") == 0)
 		vars->calculate = &calculate_burningship;
 	else if (ft_strcmp((ft_strtolower(vars->set)), "julia") == 0)
 		parse_julia(argc, argv, vars);
@@ -40,7 +40,7 @@ static void	parse_julia(int argc, char **argv, t_vars *vars)
 		vars->points.real = 0.0;
 		vars->points.imaginary = 0.0;
 	}
-	else if (argc == 3 || argc == 4 || argc > 5)
+	else if (argc == 3 || argc == 4)
 		print_help();
 	else if (!(ft_strcmp(argv[3], "+") == 0))
 		print_help();
@@ -50,8 +50,8 @@ static void	parse_julia(int argc, char **argv, t_vars *vars)
 	{
 		vars->points.real = ft_atof(argv[2]);
 		vars->points.imaginary = ft_atof(argv[4]);
-		if (vars->points.real > 2 || vars->points.real < -2
-			|| vars->points.imaginary > 1 || vars->points.imaginary < -1)
+		if (vars->points.real > 2.0 || vars->points.real < -2.0
+			|| vars->points.imaginary > 1.5 || vars->points.imaginary < -1.5)
 			print_help();
 	}
 	vars->calculate = &calculate_julia;
@@ -60,11 +60,15 @@ static void	parse_julia(int argc, char **argv, t_vars *vars)
 static void	print_help(void)
 {
 	printf("\n[INPUT ERROR]\n\n");
-	printf("Fractals sets available: Mandelbrot, BurningShip and Julia\n\n");
+	printf("Available fractals: Mandelbrot, Burning Ship and Julia.\n\n");
 	printf("Please, enter a valid input. Follow the examples:\n\n");
 	printf("Ex: ./fractol mandelbrot\n");
-	printf("Ex: ./fractol burningship\n");
-	printf("Ex: ./fractol julia\n\n");
+	printf("Ex: ./fractol burning-ship\n");
+	printf("Ex: ./fractol julia\n");
+	printf("Ex: ./fractol julia 0.355 + 0.355i\n\n");
+	printf("Obs: For Julia Set, you should pass a formula, which the first argument is the real part, which is a");
+	printf(" number between 2 and -2, followed with a \"+\" sign, and the second argument is the imaginary part which is a number between");
+	printf(" 1.5 and -1.5 followed with \"i\" which is the imaginary sign.\n");
 	printf("Obs: Input is not case sensitive.\n\n");
 	exit(0);
 }
--- srcs/set.c
+++ srcs/set.c
@@ -6,7 +6,7 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 00:40:03 by gandrade          #+#    #+#             */
-/*   Updated: 2021/10/05 21:40:50 by gandrade         ###   ########.fr       */
+/*   Updated: 2021/10/08 17:20:03 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ void	set_color(t_vars *vars)
 void	set_limits(t_vars *vars)
 {
 	if (ft_strcmp(vars->set, "mandelbrot") == 0
-		|| ft_strcmp(vars->set, "burningship") == 0)
+		|| ft_strcmp(vars->set, "burning-ship") == 0)
 	{
 		vars->points.x_max = 1.0;
 		vars->points.x_min = -2.5;
-		vars->points.y_max = 1.1;
-		vars->points.y_min = -1.1;
+		vars->points.y_max = 1.5;
+		vars->points.y_min = -1.5;
 	}
 	if (ft_strcmp(vars->set, "julia") == 0)
 	{
 		vars->points.x_max = 2.0;
 		vars->points.x_min = -2.0;
-		vars->points.y_max = 1.2;
-		vars->points.y_min = -1.2;
+		vars->points.y_max = 1.5;
+		vars->points.y_min = -1.5;
 	}
 }
