503b49203cd60af6a415f6e2a2dced4701a0ccd9
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2021-10-20 22:58:56 -0300

    fix: print help

--- srcs/print_help.c
+++ srcs/print_help.c
@@ -6,7 +6,7 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 12:57:42 by gandrade          #+#    #+#             */
-/*   Updated: 2021/10/18 22:40:19 by gandrade         ###   ########.fr       */
+/*   Updated: 2021/10/20 22:55:19 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	print_help(void)
 	printf("Ex: ./fractol julia\n");
 	printf("Ex: ./fractol julia <real> <sign> <imaginary>\n");
 	printf("    ./fractol julia 0.355 + 0.355i\n");
-	printf("    ./fractol julia -0.355 - 0.355i\n\n");
+	printf("    ./fractol julia -0.4 + 0.6i\n\n");
 	printf("Obs: Input is not case sensitive;\n");
 	printf("     Real part sign is optional;\n");
 	printf("     Imaginary part sign is the middle one, and it's mandatory;\n");
